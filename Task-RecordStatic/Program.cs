109cab0720dbb0d99837fa68d30401141aca4e73
zeIbdo <ibrahimrm@code.edu.az>
2024-06-12 02:49:17 +0400

    Task completed(imo)

--- Task-RecordStatic/Program.cs
+++ Task-RecordStatic/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Task_RecordStatic
+﻿namespace Task_RecordStatic;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        User[] users= new User[3];
+        for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Enter user {i+1} email:");
+            string email = Console.ReadLine();            
+            Console.WriteLine($"Enter user {i+1} password:");
+            Ibrahim:            
+            string password = Console.ReadLine();
+            if (User.PasswordChecker(password) == false)
+                goto Ibrahim;            
+            Console.WriteLine($"Enter user {i+1} fullname:");
+            string fullName = Console.ReadLine();
+            users[i] = new(email, password, fullName);
+        }
+        int input = 5;
+        Console.WriteLine("Welcome to menu. If you want to see all users write 1");
+        Console.WriteLine("If you want to see specific user write 2");
+        Console.WriteLine("If you want to quit from menu type 0");
+        while (input != 0)
+        {
+            input = Convert.ToInt32( Console.ReadLine());
+            if (input == 1)
+            {
+                foreach(User user in users)
+                {
+                    Console.WriteLine( user.GetInfo());
+                }
+            }
+            if (input == 2)
+            {
+                Console.WriteLine("type the user id that you want to see:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(User.FindUserById(id, users));
+            }
+            Console.WriteLine("If you want to see all users write 1");
+            Console.WriteLine("If you want to see specific user write 2");
+            Console.WriteLine("If you want to quit from menu type 0");
         }
     }
 }
---
+++ Task-RecordStatic/User.cs
@@ -0,0 +1,101 @@
+﻿namespace Task_RecordStatic;
+
+internal class User
+{
+    private string _password;
+    public static int id { get; set; }
+    public int Id { get;  }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            if (PasswordChecker(value)==true)
+                _password = value;
+            
+        }
+    }
+    public User(string email,string password,string name)
+    {
+        id++;
+        Id = id;
+        Email = email;
+        Password = password;
+        FullName = name;
+    }
+    public static bool PasswordChecker(string password)
+    {
+        bool haveBigLetter = false;
+        bool haveSmallLetter = false;
+        bool haveNumber = false;
+        bool correctLength = false;
+        if (password.Length == 8)
+        {
+            correctLength = true;
+        }
+        if (password.Length > 0)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                    haveNumber = true;
+                else
+                {
+                    if (char.IsLetter(password[i]))
+                    {
+                        if (char.IsUpper(password[i]))
+                            haveBigLetter = true;
+                        else if (char.IsLower(password[i]))
+                            haveSmallLetter = true;
+                    }
+                }
+            }
+        }
+        
+        if (haveBigLetter == true && haveSmallLetter == true && haveNumber == true&&correctLength==true)
+            return true;
+        else if (haveBigLetter == false)
+        {
+            Console.WriteLine("Password must contain at least one big letter");
+            return false;
+        }
+        else if (haveSmallLetter == false)
+        {
+            Console.WriteLine("Password must contain at least one small letter");
+            return false;
+        }
+        else if (haveNumber == false)
+        {
+            Console.WriteLine("Password must contain  at least one number");
+            return false;
+        }
+        else if (correctLength == false)
+        {
+            Console.WriteLine("Length is not correct");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Password does not match what is requested");
+            return false;
+        }
+
+    }
+    public string GetInfo()
+    {
+        return $"{Id} {FullName} {Email}";
+    }
+    public static string FindUserById(int id, User[] users)
+    {
+        foreach(User user in users)
+        {
+            if (user.Id == id)
+            {
+                return $"{user.Id} {user.Email} {user.FullName}";
+            }
+        }
+        return "User did not found";
+    }
+}
